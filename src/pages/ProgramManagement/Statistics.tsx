b697c800f7e7fd7317c83c1bcaf60287fede2230
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-03-06 02:59:03 +0330

    add program participants count to statics tab

--- src/pages/ProgramManagement/Statistics.tsx
+++ src/pages/ProgramManagement/Statistics.tsx
@@ -1,10 +1,10 @@
 import {
-  Button,
   Grid,
+  Stack,
   Typography,
 } from '@mui/material';
 import { Pagination } from '@mui/material';
-import React, { useEffect, useState, FC, Fragment } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import MentorStaticsFSMCard from 'components/organisms/cards/MentorStaticsFSMCard';
@@ -13,11 +13,14 @@ import {
   getEventWorkshopsAction,
 } from 'redux/slices/events';
 import { addMentorToWorkshopAction } from 'redux/slices/events';
+import { toPersianNumber } from 'utils/translateNumber';
 
 function Statics({
   getEventWorkshops,
   workshopsCount,
   allEventWorkshops,
+
+  program,
 }) {
   const { programId } = useParams();
   const [pageNumber, setPageNumber] = useState(1);
@@ -27,59 +30,51 @@ function Statics({
   }, [pageNumber]);
 
   return (
-    <Grid
-      container
-      item
-      spacing={2}
-      alignItems="center"
-      justifyContent="center"
-      direction="row">
+    <Stack spacing={3} alignItems={'start'} justifyContent={'center'} paddingTop={2}>
 
-      <Grid item container justifyContent='space-between' xs={12} spacing={2} style={{ marginTop: 2 }}>
-        <Grid item>
-          <Typography variant='h2'>
-            {'آمار'}
-          </Typography>
-        </Grid>
-      </Grid>
+      <Typography variant='h2' gutterBottom>
+        {'آمار'}
+      </Typography>
 
-      <Grid container spacing={2}
-        alignItems='stretch'
-        margin='10px 5px'
-        justifyContent="center"
-        sx={(theme) => ({
-          height: '100%',
-          justifyContent: 'start',
-          [theme.breakpoints.down('sm')]: {
-            justifyContent: 'center',
-            marginRight: "0px",
-          },
-        })}>
-        {allEventWorkshops?.map((workshop) => (
-          <Grid container item xs={12} sm={6} md={4} key={workshop.id} alignItems='center' justifyContent='center'>
-            <MentorStaticsFSMCard {...workshop} />
+      <Stack spacing={1}>
+        <Typography variant='h3' gutterBottom>
+          {'آمار دوره'}
+        </Typography>
+        <Typography variant='h5'>
+          {`تعداد افراد ثبت‌نامی : ${toPersianNumber(program?.participants_count)} نفر`}
+        </Typography>
+      </Stack>
+
+      <Stack spacing={1}>
+        <Typography variant='h3' gutterBottom>
+          {'آمار کارگاه‌ها'}
+        </Typography>
+        <Stack>
+          <Grid container spacing={2} alignItems='center' justifyContent="center">
+            {allEventWorkshops?.map((workshop) => (
+              <Grid item xs={12} sm={6} md={4} key={workshop.id} alignItems='center' justifyContent='center'>
+                <MentorStaticsFSMCard {...workshop} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </Stack>
+        <Pagination
+          variant="outlined"
+          color="primary"
+          shape='rounded'
+          count={Math.ceil(workshopsCount / ITEMS_PER_PAGE_NUMBER)}
+          page={pageNumber}
+          onChange={(e, value) => setPageNumber(value)}
+        />
+      </Stack>
 
-      <Grid item container>
-        <Grid item>
-          <Pagination
-            variant="outlined"
-            color="primary"
-            shape='rounded'
-            count={Math.ceil(workshopsCount / ITEMS_PER_PAGE_NUMBER)}
-            page={pageNumber}
-            onChange={(e, value) => setPageNumber(value)}
-          />
-        </Grid>
-      </Grid>
-    </Grid>
+    </Stack>
   );
 }
 const mapStateToProps = (state) => ({
   workshopsCount: state.events.workshopsCount,
   allEventWorkshops: state.events.workshops,
+  program: state.events.event,
 });
 
 export default connect(mapStateToProps, {
