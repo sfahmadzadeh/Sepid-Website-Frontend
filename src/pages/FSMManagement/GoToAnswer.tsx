2aab6ac5b62a0d5f8b0b6b96a2224b6345d29c51
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-03-11 18:32:01 +0330

    refactor file extension (convert to .tsx)

--- src/pages/FSMManagement/GoToAnswer.tsx
+++ src/pages/FSMManagement/GoToAnswer.tsx
@@ -7,30 +7,30 @@ import {
 } from '@mui/material';
 import React, { useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
-import isNumber from '../../utils/validators/isNumber';
+import isNumber from 'utils/validators/isNumber';
 
 const GoToAnswer = () => {
   const navigate = useNavigate();
   const { programId, fsmId } = useParams();
-  const [answerId, setAnswerId] = useState();
+  const [answerId, setAnswerId] = useState<number>(null);
 
   return (
     <Grid
       container item
-      justify='center'
+      justifyContent='center'
       alignItems='center'
       xs={12} sm={8} md={4}>
       <Grid item xs={12}>
         <Paper sx={{ padding: 2 }}>
           <Grid item container>
-            <Grid container item justify='center' spacing={2}>
+            <Grid container item justifyContent='center' spacing={2}>
               <Grid item xs={12}>
                 <Typography gutterBottom variant='h2' align='center'>
                   {'شناسه پاسخ خود را وارد کنید'}
                 </Typography>
               </Grid>
               <Grid item xs={12}>
-                <TextField fullWidth label='شناسه' value={answerId} onChange={(e) => isNumber(e.target.value) ? setAnswerId(e.target.value) : {}} />
+                <TextField fullWidth label='شناسه' value={answerId} onChange={(e) => isNumber(e.target.value) ? setAnswerId(parseInt(e.target.value)) : {}} />
               </Grid>
               <Grid item xs={12}>
                 <Button variant='contained' fullWidth onClick={() => { navigate(`/program/${programId}/fsm/${fsmId}/manage/correction/${answerId}/`) }}>
