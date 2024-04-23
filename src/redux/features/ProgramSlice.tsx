8737217abe49a6a2af96e1f1c5fa4fff54fc1af2
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-04-24 01:08:01 +0330

    fix bug in fetching programs

--- src/redux/features/ProgramSlice.tsx
+++ src/redux/features/ProgramSlice.tsx
@@ -16,7 +16,7 @@ export const ProgramSlice = ManageContentServiceApi.injectEndpoints({
   endpoints: builder => ({
     getPrograms: builder.query<GetProgramsOutputType, GetProgramsInputType>({
       providesTags: ['programs'],
-      query: ({ websiteName, pageNumber = 1, isPrivate }) => `fsm/event/?website=${websiteName}&page=${pageNumber}${isPrivate ? `&is_private=${isPrivate}` : ''}`,
+      query: ({ websiteName, pageNumber = 1, isPrivate }) => `fsm/event/?website=${websiteName}&page=${pageNumber}${isPrivate != null ? `&is_private=${isPrivate}` : ''}`,
       transformResponse: (respons: any): GetProgramsOutputType => {
         return {
           programs: respons.results,
