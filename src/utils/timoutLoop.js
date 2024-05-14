fa5fc57efcefef821410fc41e0d6ac916b06fffc
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-05-14 18:17:46 +0330

    remove redundant utilities

--- src/utils/timoutLoop.js
+++
@@ -1,9 +0,0 @@
-export default function timeoutLoop (count, delay, cb) {
-  for (let i = 0; i < count; i++) {
-    (function (ind) {
-      setTimeout(function () {
-        cb(ind)
-      }, delay * ind)
-    })(i)
-  }
-}
