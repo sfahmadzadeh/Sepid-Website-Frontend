32333b18c5a552b1870f7b15ebf663ad5477954b
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-05-15 19:37:12 +0330

    remove name from website logo + remove Lalezar font

--- src/components/organisms/Appbar/components/logos/LogoSkeleton.tsx
+++ src/components/organisms/Appbar/components/logos/LogoSkeleton.tsx
@@ -5,7 +5,6 @@ const LogoSkeleton = () => {
   return (
     <Stack direction='row' alignItems={'center'} justifyContent={'center'} sx={{ userSelect: 'none' }} spacing={1}>
       <Skeleton variant="circular" width={50} height={50} />
-      <Skeleton variant="rounded" width={150} height={50} />
     </Stack>
   );
 }
--- src/components/organisms/Appbar/components/logos/WebsiteLogo.tsx
+++ src/components/organisms/Appbar/components/logos/WebsiteLogo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Stack, Typography } from "@mui/material";
+import { IconButton } from "@mui/material";
 import { useGetWebsiteQuery } from "redux/features/WebsiteSlice";
 import LogoSkeleton from "./LogoSkeleton";
 import { Link } from "react-router-dom";
@@ -17,14 +17,9 @@ const WebsiteLogo = () => {
 
   return (
     // todo: change /programs/ to /dashboard/
-    <Button sx={{ padding: 0, paddingX: 1 }} disableRipple component={Link} to='/programs/'>
-      <Stack direction='row' alignItems={'center'} justifyContent={'center'} sx={{ userSelect: 'none' }} spacing={1}>
-        <img alt="website-logo" src={website.logo.desktop_image} width={50} />
-        <Typography fontFamily={"Lalezar"} sx={{ color: "#3498DB", fontSize: { xs: 36, md: 40 }, fontWeight: 500 }}>
-          {website.display_name}
-        </Typography>
-      </Stack>
-    </Button>
+    <IconButton sx={{ padding: 0, paddingX: 1, userSelect: 'none' }} disableRipple component={Link} to='/programs/'>
+      <img alt="website-logo" src={website.logo.desktop_image} width={50} />
+    </IconButton>
   );
 }
 
