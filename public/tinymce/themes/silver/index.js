ee775ce0ee96b2cae3ec855c86b2622022c69024
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-06-04 04:23:23 +0330

    fix sentry building bug

--- public/tinymce/themes/silver/index.js
+++ public/tinymce/themes/silver/index.js
@@ -5,3 +5,5 @@
 //   ES2015:
 //     import 'tinymce/themes/silver'
 require('./theme.min.js');
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="532e9044-62b5-5d21-a347-ecd841e7fea1")}catch(e){}}();
+//# debugId=532e9044-62b5-5d21-a347-ecd841e7fea1
