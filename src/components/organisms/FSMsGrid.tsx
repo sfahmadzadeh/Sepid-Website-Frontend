136c33afb35f8139e7095e55a13f142276ad1270
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-06-05 03:49:30 +0330

    fix a minor bug in FSMsGrid

--- src/components/organisms/FSMsGrid.tsx
+++ src/components/organisms/FSMsGrid.tsx
@@ -19,7 +19,7 @@ const FSMsGrid: FC<FSMsGridPropsType> = ({
   const { data: fsmsData, isLoading } = useGetFSMsQuery({ programId, pageNumber })
 
   const numberOfSkeleton = width === 'sm' || width === 'md' ? 4 : 3;
-  
+
   const visibleFSMS = fsmsData?.fsms?.filter(fsm => fsm.is_visible) || []
 
   if (isLoading) {
@@ -37,14 +37,16 @@ const FSMsGrid: FC<FSMsGridPropsType> = ({
   if (visibleFSMS.length > 0) {
     let tmpArr = [...visibleFSMS].filter(fsm => fsm.is_visible).sort((fsm1, fsm2) => fsm2.order_in_program - fsm1.order_in_program)
     return (
-      <Stack>
-        <Grid container spacing={2}>
-          {tmpArr.map((fsm) => (
-            <Grid item key={fsm.id} xs={12} sm={6} lg={4}>
-              <FSMCard fsm={fsm} />
-            </Grid>
-          ))}
-        </Grid>
+      <Stack spacing={2}>
+        <Stack>
+          <Grid container spacing={2}>
+            {tmpArr.map((fsm) => (
+              <Grid item key={fsm.id} xs={12} sm={6} lg={4}>
+                <FSMCard fsm={fsm} />
+              </Grid>
+            ))}
+          </Grid>
+        </Stack>
         <Pagination
           variant="outlined"
           color="primary"
