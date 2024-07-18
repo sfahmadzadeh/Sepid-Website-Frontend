2994b73ad7c41ec86812d22b0045eb263bf67eb1
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-07-18 14:37:41 +0330

    fix: fix a minor bug

--- src/components/organisms/Appbar/components/NotificationButton.tsx
+++ src/components/organisms/Appbar/components/NotificationButton.tsx
@@ -24,7 +24,7 @@ const NotificationButton: FC<NotificationButtonPropsType> = ({
   const { data: website } = useGetWebsiteQuery();
   const userInfo = useSelector((state: any) => state.account.userInfo);
   const { data: userProfileSummary } = useGetPartyProfileSummaryQuery({ partyId: userInfo.id });
-  const room = (website.name && userProfileSummary?.id) ? `sepid-${website.name}-${userProfileSummary?.id}` : null;
+  const room = (website?.name && userProfileSummary?.id) ? `sepid-${website.name}-${userProfileSummary?.id}` : null;
   // const sendMessage = useWebSocket({ room });
   const [message, setMessage] = useState('');
   const messages = useSelector((state: any) => state.websocket.messages);
