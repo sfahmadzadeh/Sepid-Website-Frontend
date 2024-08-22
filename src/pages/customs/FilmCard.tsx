3db5e0d5b5a6f8ded8f103c9ff9e50766d42af4a
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-08-22 18:47:36 +0330

    feat: add discountCode dialog for FilmCard

--- src/pages/customs/FilmCard.tsx
+++ src/pages/customs/FilmCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -7,6 +7,7 @@ import Chip from '@mui/material/Chip';
 import Box from '@mui/material/Box';
 import MovieIcon from '@mui/icons-material/Movie';
 import { Button, CardActions } from '@mui/material';
+import DiscountDialog from './DIscounCodeDialog';
 
 type FilmCardPropsType = {
   filmName: string;
@@ -23,36 +24,42 @@ const FilmCard: React.FC<FilmCardPropsType> = ({
   director,
   description,
 }) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  const onButtonClick = () => {
+  const handleOpenDialog = () => {
+    setIsDialogOpen(true);
+  };
 
-  }
+  const handleCloseDialog = () => {
+    setIsDialogOpen(false);
+  };
 
   return (
-    <Card sx={{
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-      maxWidth: 345,
-      boxShadow: 3
-    }}>
-      <CardMedia
-        component="img"
-        height="300"
-        image={picture}
-        alt={filmName}
-      />
-      <CardContent sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', paddingBottom: 1 }}>
-        <Typography gutterBottom variant="h5" component="div">
-          {filmName}
-        </Typography>
-        <Typography variant="subtitle1" color="text.secondary" gutterBottom>
-          فیلمی از {director}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" paragraph>
-          {description}
-        </Typography>
-        {/* <Box display="flex" flexWrap="wrap" gap={1} mt={2}>
+    <Fragment>
+      <Card sx={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        maxWidth: 345,
+        boxShadow: 3
+      }}>
+        <CardMedia
+          component="img"
+          height="300"
+          image={picture}
+          alt={filmName}
+        />
+        <CardContent sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', paddingBottom: 1 }}>
+          <Typography gutterBottom variant="h5" component="div">
+            {filmName}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" gutterBottom>
+            فیلمی از {director}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" paragraph>
+            {description}
+          </Typography>
+          {/* <Box display="flex" flexWrap="wrap" gap={1} mt={2}>
           {releasedCities.map((city) => (
             <Chip
               key={city}
@@ -64,13 +71,19 @@ const FilmCard: React.FC<FilmCardPropsType> = ({
             />
           ))}
         </Box> */}
-      </CardContent>
-      <CardActions>
-        <Button variant='contained' fullWidth color="primary" onClick={onButtonClick}>
-          {'دریافت کد تخفیف'}
-        </Button>
-      </CardActions>
-    </Card>
+        </CardContent>
+        <CardActions>
+          <Button variant='contained' fullWidth color="primary" onClick={handleOpenDialog}>
+            {'دریافت کد تخفیف'}
+          </Button>
+        </CardActions>
+      </Card>
+      <DiscountDialog
+        open={isDialogOpen}
+        onClose={handleCloseDialog}
+        filmName={filmName}
+      />
+    </Fragment>
   );
 };
 
